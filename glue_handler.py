f1d8c6958511e67e7475cbee0d4521d84ab7aa0a
sushmagit18 <gmsusshma2023@gmail.com>
2025-04-04 18:34:28 -0400

    add

--- glue_handler.py
+++
@@ -1,8 +0,0 @@
-import json
-import boto3
-
-glue = boto3.client('glue')
-
-def lambda_handler(event, context):
-    response = glue.start_job_run(JobName='HelloworldGlueJob')
-    return {"statusCode": 200, "body": json.dumps(f"Glue job started: {response['JobRunId']}")}
